8f72394ceb17ac3a9573c861d7bbe8dc75fc99cb
azlfa <azlfauniversity@gmail.com>
2025-03-28 16:28:39 -0600

    finishing courses

--- my-lms-app/src/data/courses.js
+++ my-lms-app/src/data/courses.js
@@ -9,73 +9,73 @@ const courses = [
   },
   {
     id: 2,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
+    name: "Data Structures and Algorithms",
+    instructor: "Dr. Bridget Jones",
+    description: "Learn the different types of DSA",
+    duration: "8 weeks",
+    image: "images/course2.jpg"
+  },
+  {
+    id: 3,
+    name: "Full Stack Web Development",
+    instructor: "Dr. Novarun Deb",
     description: "Master HTML, CSS, and JavaScript.",
     duration: "8 weeks",
     image: "images/course1.jpg"
   },
   {
-    id: 1,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
-    description: "Master HTML, CSS, and JavaScript.",
+    id: 4,
+    name: "Computer Architectures",
+    instructor: "Dr. Norm Bartley",
+    description: "Learning ARM and assembly.",
+    duration: "8 weeks",
+    image: "images/course2.jpg"
+  },
+  {
+    id: 5,
+    name: "Multivariable Calculus",
+    instructor: "Dr. Safaa Tazin",
+    description: "Dive deeper into eigenvalues, Fouriers Law and more.",
     duration: "8 weeks",
     image: "images/course1.jpg"
   },
   {
-    id: 1,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
-    description: "Master HTML, CSS, and JavaScript.",
+    id: 6,
+    name: "Physics II",
+    instructor: "Dr. Sean Stotyn",
+    description: "Acoustics, Optics and Radiation.",
+    duration: "8 weeks",
+    image: "images/course2.jpg"
+  },
+  {
+    id: 7,
+    name: "Statics",
+    instructor: "Dr. Ahmed Ghasemloonia",
+    description: "Beams, Forces and more.",
     duration: "8 weeks",
     image: "images/course1.jpg"
   },
   {
-    id: 1,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
-    description: "Master HTML, CSS, and JavaScript.",
+    id: 8,
+    name: "Object Oriented Programming",
+    instructor: "Dr. Ann Barcomb",
+    description: "Master OOP with Java.",
+    duration: "8 weeks",
+    image: "images/course2.jpg"
+  },
+  {
+    id: 9,
+    name: "Networked Systems",
+    instructor: "Dr. Faiza Mazhar",
+    description: "Introduction to software-defined networks.",
     duration: "8 weeks",
     image: "images/course1.jpg"
   },
   {
-    id: 1,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
-    description: "Master HTML, CSS, and JavaScript.",
-    duration: "8 weeks",
-    image: "images/course1.jpg"
-  },
-  {
-    id: 1,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
-    description: "Master HTML, CSS, and JavaScript.",
-    duration: "8 weeks",
-    image: "images/course1.jpg"
-  },
-  {
-    id: 1,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
-    description: "Master HTML, CSS, and JavaScript.",
-    duration: "8 weeks",
-    image: "images/course1.jpg"
-  },
-  {
-    id: 1,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
-    description: "Master HTML, CSS, and JavaScript.",
-    duration: "8 weeks",
-    image: "images/course1.jpg"
-  },
-  {
-    id: 1,
-    name: "Web Development",
-    instructor: "Dr. John Smith",
-    description: "Master HTML, CSS, and JavaScript.",
+    id: 10,
+    name: "Operating Systems",
+    instructor: "Dr. Gina Linetti",
+    description: "Processes, threads and CPU scheduling.",
     duration: "8 weeks",
     image: "images/course1.jpg"
   },
