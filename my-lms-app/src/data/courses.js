c88a7452dd50eb67408eadf7639c62e2d4814743
azlfa <azlfauniversity@gmail.com>
2025-03-28 06:38:50 -0600

    comp

---
+++ my-lms-app/src/data/courses.js
@@ -0,0 +1,85 @@
+const courses = [
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 2,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  {
+    id: 1,
+    name: "Web Development",
+    instructor: "Dr. John Smith",
+    description: "Master HTML, CSS, and JavaScript.",
+    duration: "8 weeks",
+    image: "images/course1.jpg"
+  },
+  
+];
+
+export default courses;
