f35bf03157ebd17586b1182707f9a9f168b8af59
azlfa <azlfauniversity@gmail.com>
2025-03-28 16:46:07 -0600

    testimonials

---
+++ my-lms-app/src/data/testimonials.js
@@ -0,0 +1,27 @@
+const testimonials = [
+  {
+    studentName: "Alice Johnson",
+    courseName: "Web Development",
+    review: "Excellent course structure!",
+    rating: 5
+    },
+  {
+    studentName: "Abdullah Anwar",
+    courseName: "Computer Architecture",
+    review: "Pretty tough!",
+    rating: 3
+    },
+  {
+    studentName: "Hafsa Ghazanfar",
+    courseName: "Multiariable calculus",
+    review: "The professor made it very easy.",
+    rating: 5
+  },
+  {
+    studentName: "Mantasha Azam",
+    courseName: "Networked Systems",
+    review: "Challenging but rewarding.",
+    rating: 5
+    },
+  ];
+  export default testimonials;
