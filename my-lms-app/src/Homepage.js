a2923947f2b67f74f504984f855cb3d5704315bd
azlfa <azlfauniversity@gmail.com>
2025-03-29 17:07:46 -0600

    cleanup

---
+++ my-lms-app/src/Homepage.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
+import Footer from "./components/Footer";
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <MainSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
